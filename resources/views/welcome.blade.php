ffc103e724753dcfe9cee84ca57f9efae7ab9a80
iruadib <khoiruladib422@gmail.com>
2022-01-14 05:15:45 +0700

    add meta tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <meta name="description" content="React App">
   <meta property="og:title" content="React App">
   <meta property="og:type" content="storage" />
   <meta property="og:image" content="http://127.0.0.1:8000/favicon.ico">
